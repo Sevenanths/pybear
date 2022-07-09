0e83fdbaee9beb9c339d32b4c452ce0791a4606c
Sevenanths <anthernate@gmail.com>
2022-07-09 11:29:47 +0000

    More comments and constants

--- bear.py
+++ bear.py
@@ -1,17 +1,30 @@
+# pybear
+# by Anthe
+# started on 2022-07-09
+
+# -- Imports --
 import pygame
 import os
 pygame.font.init()
 pygame.mixer.init()
 
+# -- Constants --
 WIDTH, HEIGHT = 800, 480
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("pybear")
 
+OBJECT_WIDTH = 32
+OBJECT_HEIGHT = 32
+
 FPS = 60
+
+# -- Assets --
+SPR_WALL = pygame.image.load(os.path.join('assets', 'wall.png'))
 
 BG = pygame.transform.scale(pygame.image.load(
     os.path.join('assets', 'bg.jpg')), (WIDTH, HEIGHT))
 
+# -- Functions --
 def draw_window():
 	WIN.blit(BG, (0, 0))
 
